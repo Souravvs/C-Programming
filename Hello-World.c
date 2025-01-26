646805333e7d18bfd4d681e08404e81d7392f4cc
Sourav <souravvs2028@cs.sjcetpalai.ac.in>
2025-01-26 19:02:47 +0530

    Add files via upload

---
+++ Hello-World.c
@@ -0,0 +1,11 @@
+/*
+Author: Sourav V S
+Date: 26-01-2025
+Description: This is a program to print "Hello World!"
+*/
+
+#include<stdio.h>
+int main(){
+    printf("\nHello World!");
+    return 0;
+}
