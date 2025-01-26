a6cb18d9f3806e943c38a49c89ac4bed02781f07
Sourav <souravvs2028@cs.sjcetpalai.ac.in>
2025-01-26 19:04:20 +0530

    Add files via upload

---
+++ Sum.c
@@ -0,0 +1,15 @@
+/*
+Author: Sourav V S
+Date: 26-01-2025
+Description: This is a program to print "Hello World!"
+*/
+
+#include<stdio.h>
+int main() {
+    int num1,num2,sum;
+    printf("\nEnter two numbers: ");
+    scanf("%d%d",&num1,&num2);
+    sum = num1 + num2;
+    printf("\nSum of two numbers: %d",sum);
+    return 0;
+}
