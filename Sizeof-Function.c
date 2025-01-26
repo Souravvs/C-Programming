3ac818e3b1ef3f044dbfc840511aece1b0a31513
Sourav <souravvs2028@cs.sjcetpalai.ac.in>
2025-01-26 19:20:11 +0530

    Add files via upload

---
+++ Sizeof-Function.c
@@ -0,0 +1,15 @@
+/*
+Author: Sourav V S
+Date: 26-01-2025
+Description: This is a program about sizeof() function.
+*/
+
+#include<stdio.h>
+int main(){
+    printf("\nSize of Integer: %ld",sizeof(int));
+    printf("\nSize of Long Integer: %ld",sizeof(long int));
+    printf("\nSize of Float: %ld",sizeof(float));
+    printf("\nSize of Double: %ld",sizeof(double));
+    printf("\nSize of Char: %ld",sizeof(char));
+    return 0;
+}
