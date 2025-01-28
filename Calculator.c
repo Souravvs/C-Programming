363bbbb3366f437102f682abd4872c61067de31b
Sourav <souravvs2028@cs.sjcetpalai.ac.in>
2025-01-28 21:37:16 +0530

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,34 @@
+/*
+Author: Sourav V S
+Date: 28-01-2025
+Description: This program takes two integer operands and one operator from the 
+user, performs the operation and then prints the result.
+*/
+
+#include<stdio.h>
+int main(){
+    int num1, num2;
+    char operation;
+    printf("\nEnter two numbers: \n");
+    scanf("%d%d",&num1,&num2);
+    printf("\nChoose an operation:\nAddition (A)\nSubtraction (S)\nMultiplication (M)\nDivision (D)\n");
+    scanf(" %c",&operation);
+    switch(operation){
+        case 'A': 
+            printf("\nResult: %d", num1 + num2);
+            break;
+        case 'S': 
+            printf("\nResult: %d", num1 - num2);
+            break;
+        case 'M':  
+            printf("\nResult: %d", num1 * num2);
+            break;
+        case 'D': 
+            printf("\nResult: %d", num1 / num2);
+            break;
+        default:
+            printf("\nInvalid input!");
+            break;
+    }
+    return 0;
+}
