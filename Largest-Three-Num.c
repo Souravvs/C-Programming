69896a7eb6fd2f200e76ad68b3ab902cd908de5e
Sourav <souravvs2028@cs.sjcetpalai.ac.in>
2025-01-29 14:36:11 +0530

    Add files via upload

---
+++ Largest-Three-Num.c
@@ -0,0 +1,34 @@
+/*
+Author: Sourav V S
+Date: 29-01-2025
+Description: This is a program to input three numbers and find the largest among them using nested if statements. 
+*/
+
+#include<stdio.h>
+int main(){
+  int num1,num2,num3;
+  printf("\nEnter three numbers: \n");
+  scanf("%d %d %d",&num1, &num2, &num3);
+  if(num1 > num2){
+    if(num1 > num3){
+      printf("\nThe largest number is %d\n",num1);
+    }else{
+      printf("\nThe largest number is %d\n",num3);
+    }
+  }else if(num2 > num1){
+    if(num2 > num3){
+      printf("\nThe largest number is %d\n",num2);
+    }else{
+      printf("\nThe largest number is %d\n",num3);
+    }
+  }else if(num3 > num1){
+    if(num3 > num2){
+      printf("\nThe largest number is %d\n",num3);
+    }else{
+      printf("\nThe largest number is %d\n",num2);
+    }
+  }else{
+    printf("\nThe numbers are equal\n");
+  }
+  return 0;
+}
