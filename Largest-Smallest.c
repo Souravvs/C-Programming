c4ab97bdd49adf46f445df11847aed5be01fd118
Sourav <souravvs2028@cs.sjcetpalai.ac.in>
2025-02-05 16:25:31 +0530

    Add files via upload

---
+++ Largest-Smallest.c
@@ -0,0 +1,31 @@
+/*
+Author: Sourav V S
+Date: 05-02-2025
+Description: This is a program to perform a sequential search in a one-dimensional array and find the maximum and minimum elements. Display their positions in the array.
+*/
+#include<stdio.h>
+int main(){
+  int n,i,j,temp;
+  printf("\nEnter the limit: ");
+  scanf("%d", &n);
+  int num[n];
+  printf("\nEnter the elements: ");
+  for(i=0; i<n; i++){
+    scanf("%d", &num[i]);
+  }
+  //Bubble sort algorithm
+  for(i=0; i<n-1; i++){
+    for(j=0; j<n-1-i; j++){
+      if(num[j] > num[j+1]){
+        temp = num[j];
+        num[j] = num[j+1];
+        num[j+1] = temp;
+      }
+    }
+  }
+  printf("\nThe sorted array is: ");
+  for(i=0; i<n; i++){
+    printf("%d ",num[i]);
+  }
+  return 0;
+}
