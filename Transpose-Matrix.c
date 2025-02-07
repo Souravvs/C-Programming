81d4533d55426eeac522c4a58a55ff3122434b67
Sourav <souravvs2028@cs.sjcetpalai.ac.in>
2025-02-07 10:38:55 +0530

    Add files via upload

---
+++ Transpose-Matrix.c
@@ -0,0 +1,33 @@
+/*
+Author: Sourav V S
+Date: 07-02-25
+Description: This is a program to print the transpose of a matrix.
+*/
+#include <stdio.h>
+int main(){
+  int row,col;
+  printf("\nEnter the rows and columns: ");
+  scanf("%d %d",&row,&col);
+  int num[row][col];
+  printf("\nEnter the elements: ");
+  for (int i=0; i<row; i++){
+    for (int j=0; j<col; j++){
+      scanf("%d",&num[i][j]);
+    }
+  }
+  printf("\nThe matrix is: \n");
+  for (int i=0; i<row; i++){
+    for (int j=0; j<col; j++){
+      printf("%d\t",num[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\nThe transpose of the matrix: \n");
+  for (int i=0; i<row; i++){
+    for (int j=0; j<col; j++){
+      printf("%d\t",num[j][i]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
